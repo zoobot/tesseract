db93539b50ce7538df2fdc5b19a270cb43868424
Charleen Fei <fei.charleen@gmail.com>
2016-12-27 13:40:37 +0100

    'modified Dockerfiles to use temp express server'

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path= require ('path');
 
 module.exports = {
     entry: {
-      entry: './client/client.js',
+      // entry: './client/client.js',
     },
     output: {
       path: path.resolve('dist'),
