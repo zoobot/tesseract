164c1e05fcef02df3c92ed8df92365d5cb125d00
Duncan Nevin <duncan.nevin@gmail.com>
2017-01-05 12:14:26 -0800

    Some work on db connection. Not working

--- editor/db.go
+++ editor/db.go
@@ -4,39 +4,83 @@ import (
   "fmt"
   "net/http"
   "gopkg.in/mgo.v2"
-  // "gopkg.in/mgo.v2/bson"
+  "gopkg.in/mgo.v2/bson"
 )
+/* ======>User Schema<====== */
+type User struct {
+  id       bson.ObjectId `bson:"_id,omitempty"`
+  userName string
+  password string
+}
 
+var (
+  IsDrop = true
+)
 /* ======>Database Handlers<====== */
-func saveDoc(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(200)
-  fmt.Fprintf(w, "This is a POST request too, /%s! ", r.URL.Path[1:])
-}
-
-func retrieveDoc(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(200)
-  fmt.Fprintf(w, "This is a GET request too, /%s! ", r.URL.Path[1:])
-}
-
-func updateDoc(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(200)
-  fmt.Fprintf(w, "This is a PUT request too, /%s! ", r.URL.Path[1:])
-}
-
-func deleteDoc(w http.ResponseWriter, r *http.Request) {
-  w.WriteHeader(200)
-  fmt.Fprintf(w, "This is a DELETE request too, /%s! ", r.URL.Path[1:])
-}
-/* <=======end Database Handlers======> */
-/* ======>Database Connection<====== */
-func getSession() *mgo.Session {
-  // Connect to out local mongo
-  s, err := mgo.Dial("mongodb://localhost")
-
-  // Check if connection error, is mongo running?
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+  session, err := mgo.Dial("mongodb://localhost:27017")
   if err != nil {
     panic(err)
   }
-  return s
+
+  c := session.DB("tesis").C("users")
+
+  err = c.Insert(&User{userName: "Ale", password: "1234"},
+    &User{userName: "Cla", password: "1234"})
+  if err != nil {
+    panic(err)
+  }
+
+  result := User{}
+
+  // err = c.Find(bson.M{}).All(&result)
+  // if err != nil {
+  //   panic(err)
+  // }
+
+  w.WriteHeader(201)
+  fmt.Fprintf(w, "POST request added, %s! ", result)
 }
-/* <=======end Database Connection======> */
+
+func GetUser(w http.ResponseWriter, r *http.Request) {
+
+  session, err := mgo.Dial("mongodb://localhost:27017")
+  if err != nil {
+    panic(err)
+  }
+
+  var results []User
+  c := session.DB("tesis").C("users")
+  err = c.Find(bson.M{}).Sort("-timestamp").All(&results)
+
+  if err != nil {
+    panic(err)
+  }
+
+  w.WriteHeader(200)
+  fmt.Fprintf(w, "GET request response, %s! ", results)
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Request) {
+
+  session, err := mgo.Dial("mongodb://localhost:27017")
+  if err != nil {
+    panic(err)
+  }
+
+  c := session.DB("tesis").C("users")
+  err = c.RemoveAll(bson.M{})
+  if err != nil {
+    panic(err)
+  }
+
+  w.WriteHeader(200)
+  fmt.Fprintf(w, "DELETE successful")
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Request) {
+
+  w.WriteHeader(200)
+  fmt.Fprintf(w, "This is a PUT request too, /%s! ", r.URL.Path[1:])
+}
+/* <=======end Database Handlers======> */
