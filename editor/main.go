e90568c8d6a0415d4389060c43d38a6800b210b7
Duncan Nevin <duncan.nevin@gmail.com>
2017-01-10 15:23:16 -0800

    Put request works. Post new file works

--- editor/main.go
+++ editor/main.go
@@ -39,6 +39,7 @@ func main() {
   /* ======>API<====== */
 
   r.HandleFunc("/db", CreateUser).Methods("POST")
+  r.HandleFunc("/db/file", AddFile).Methods("POST")
   r.HandleFunc("/db", GetUser).Methods("GET")
   r.HandleFunc("/db", UpdateUser).Methods("PUT")
   r.HandleFunc("/db", DeleteUser).Methods("DELETE")
