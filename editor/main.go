487594e41833856f007a6366019f8eac5fc25456
Duncan Nevin <duncan.nevin@gmail.com>
2017-01-10 16:40:54 -0800

    db code cleaned up, db finished

--- editor/main.go
+++ editor/main.go
@@ -39,7 +39,8 @@ func main() {
   /* ======>API<====== */
 
   r.HandleFunc("/db", CreateUser).Methods("POST")
-  r.HandleFunc("/db/file", AddFile).Methods("POST")
+  r.HandleFunc("/db/addfile", AddFile).Methods("POST")
+  r.HandleFunc("/db/deletefile", DeleteFile).Methods("POST")
   r.HandleFunc("/db", GetUser).Methods("GET")
   r.HandleFunc("/db", UpdateUser).Methods("PUT")
   r.HandleFunc("/db", DeleteUser).Methods("DELETE")
