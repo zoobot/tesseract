60eaed5220ef2843ad0ae28093b7bc0d3f75361d
botzoo <rommims@gmail.com>
2018-01-27 04:04:22 -0800

    deleter

--- test/index.js
+++
@@ -1,6 +0,0 @@
-// <!-- Created by Duncan on 12.28.2016 -->
-// test/index.js
-// require all test files using special Webpack feature
-// https://webpack.github.io/docs/context.html#require-context
-const testsContext = require.context('./client_spec', true, /\_spec$/)
-testsContext.keys().forEach(testsContext)
